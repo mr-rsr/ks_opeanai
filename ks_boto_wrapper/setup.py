0580cc2924e4de2991165e8c69f3b7ca771ac1b4
Aswin <pratapsinghaswin@gmail.com>
2025-06-09 19:15:07 +0530

    includes config files

--- ks_boto_wrapper/setup.py
+++ ks_boto_wrapper/setup.py
@@ -1,21 +1,30 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="ks_boto_wrapper",
-    version="0.1.0",
-    description="A wrapper library for boto3 utilities",
-    author="Aswin Pratapsingh",
-    author_email="pratapsinghaswin@gmail.com",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        "boto3",
-        "pydantic",
-        "requests"
+        "requests>=2.32.3",
+        "pydantic>=2.0.0",
+        "boto3"
     ],
-    python_requires=">=3.7",
+    description="This package provides convenient access to the GenAI Models' REST API from any Python 3.7+ application.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Raj Aryan",
+    author_email="raj@kloudstac.com",
+    url="https://github.com/mr-rsr/ks_opeanai",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.7",
 )
